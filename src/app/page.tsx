441031f008031f798901a8c8493e4408d32b88eb
dashdude2 <167656561+dashdude2@users.noreply.github.com>
2025-02-07 17:08:03 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+
+import Fonts from "@/components/fonts";
+
+import Hero from "@/components/Hero";
+import Products from "./products/page";
+import Top_sell from "./products/sell";
+import Dress from "@/components/dress";
+import CustomerCarousel from "@/components/couresel";
+
+
+
+export default function Home() {
+  return (
+    <div >
+       
+       <Hero/>
+       <Fonts/>
+       <Products/>
+       <Top_sell/>
+       <Dress/>
+       <CustomerCarousel/>
+    </div>  
+  );
+}
+ 
